cd03758803be987102ba40f30405806cf7cffe3d
TorCasDev <tor.cas.alejandro@gmail.com>
2025-02-06 16:41:25 +0100

    UserForm, Context & list done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,36 @@
-import './styles/index.scss'
+import { useEffect, useState } from 'react'
+import './styles/index.css'
+import { ContactsContext } from './contexts';
+import { CreateUser } from './components/CreateUser';
+import { UserCard } from './components/UserCard';
+
+const apiUrl = import.meta.env.VITE_FETCH_API_URL;
 
 function App() {
-  return <></>
+
+  const [data, setData] = useState([]);
+
+  const getData = async () => {
+    const response = await fetch(apiUrl).then(response => response.json())
+
+    console.log(JSON.stringify(response[0], null, 2))
+    setData(response)
+  }
+
+  useEffect(() => {
+    getData()
+  }, [])
+
+  return <ContactsContext.Provider value={{ data, setData }}>
+    <nav className='nav'>
+      <button>Formulario</button>
+      <button>Listado</button>
+    </nav>
+    <CreateUser />
+    <div id="contacts_wrapper">
+      {data.map((item, index) => <UserCard key={index} user={item} />)}
+    </div>
+  </ContactsContext.Provider>
 }
 
 export default App
